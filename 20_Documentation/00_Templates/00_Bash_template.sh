a2d34638fc4717511066ddd0a50fcaa91778689b
YURY TIKHOGLAZ <usetiskip@me.com>
2019-03-21 00:09:09 +0300

    initial

---
+++ 20_Documentation/00_Templates/00_Bash_template.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#--------------------------------------------------------------------------#
+#	Project			:
+#	Created by		:
+#	Creation date	:
+#	QC by			:
+#	QC date			:
+#	Input folder	:
+#	Input files		:
+#	Output folder	:
+#	Output files	:
+#	Execution folder:
+#--------------------------------------------------------------------------#
+
+# STEP 0: - STEP DESCRIPTION
+
+echo "Hello world"
+
+# EXPECTED RESULT:
+# Console Output - "Hello world"
+
+# ACTUAL RESULT  :
+
+
+#--------------------------------------------------------------------------#
+
+# STEP 1: ....
